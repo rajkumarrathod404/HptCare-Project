b7d545c5b604ad0c9b64f9d49791734aa3e2f3d6
Rajkumar Rathod <rajkumar.rathod@thinkitive.com>
2023-11-03 12:56:02 +0530

    emp table added

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,25 +1,67 @@
-import { useDisclosure } from '@mantine/hooks';
-import { AppShell, Burger, Group, Skeleton } from '@mantine/core';
+import { useDisclosure } from "@mantine/hooks";
+import {
+  AppShell,
+  Box,
+  Burger,
+  Button,
+  Group,
+  Skeleton,
+  Table,
+  TableTbody,
+  TableThead,
+} from "@mantine/core";
+import { useEffect, useState } from "react";
 // import { MantineLogo } from '@mantine/ds';
-
+interface empType {
+  id: number;
+  name: string;
+  email: string;
+  num: number;
+}
 export function CollapseDesktop() {
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+  const [empList, setEmpList] = useState<empType[]>([]);
+  const handleDelete = (id: number, source: string) => {
+    // source==="ADD"?
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:5174/emp")
+      .then((res) => {
+        return res.json();
+      })
+      .then((resp) => {
+        setEmpList(resp);
+      })
+      .catch((err: string) => {
+        console.log("json error", err);
+      });
+  }, []);
 
   return (
     <AppShell
       header={{ height: 60 }}
       navbar={{
         width: 300,
-        breakpoint: 'sm',
+        breakpoint: "sm",
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
-      padding="md"
-    >
+      padding="md">
       <AppShell.Header>
         <Group h="100%" px="md">
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="md" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="md" />
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="md"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="md"
+          />
           Profile
           {/* <MantineLogo size={30} /> */}
         </Group>
@@ -32,7 +74,45 @@ export function CollapseDesktop() {
             <Skeleton key={index} h={28} mt="md" animate={false} />
           ))}
       </AppShell.Navbar>
-      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Main>
+        <Table>
+          <TableThead>
+            <tr>
+              <td>Id</td>
+              <td>Name</td>
+              <td>email</td>
+              <td>Contact</td>
+              <td>Action</td>
+            </tr>
+          </TableThead>
+          {empList?.map((emp: empType) => {
+            return (
+              <TableTbody>
+                <tr key={emp.id}>
+                  <td>{emp.id}</td>
+                  <td>{emp.name}</td>
+                  <td>{emp.email}</td>
+                  <td>{emp.num}</td>
+                  <td>
+                    <Button
+                      onClick={() => {
+                        handleDelete(emp.id, "ADD");
+                      }}>
+                      Add
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        handleDelete(emp.id, "REMOVE");
+                      }}>
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              </TableTbody>
+            );
+          })}
+        </Table>
+      </AppShell.Main>
     </AppShell>
   );
 }
